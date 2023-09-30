d0dcc30f6c9936b825ff3579358db1bac89d1c20
MinhTam <lait2553@gmail.com>
2023-09-30 22:43:28 +0700

    feat: Implement SquareOTPInput component for OTP entry and add screen1_c component

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import firstScreen from './screens/firstScreen';
 import screen1_a from './screens/screen1_a';
 import screen1_b from './screens/screen1_b';
+import screen1_c from './screens/screen1_c';
 
 const Stack = createNativeStackNavigator();
 
@@ -11,7 +12,7 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName='firstScreen' screenOptions={{ headerShown: false }}>
-        <Stack.Screen name='firstScreen' component={screen1_b} />
+        <Stack.Screen name='firstScreen' component={screen1_c} />
       </Stack.Navigator>
     </NavigationContainer>
   );
