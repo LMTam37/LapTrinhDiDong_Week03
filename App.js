02be1ef31607d0a973b6be71b475028c45dd5c0b
MinhTam <lait2553@gmail.com>
2023-09-30 19:37:29 +0700

    feat: Add screen1_b component

--- App.js
+++ App.js
@@ -3,12 +3,15 @@ import { NavigationContainer } from '@react-navigation/native';
 
 import firstScreen from './screens/firstScreen';
 import screen1_a from './screens/screen1_a';
+import screen1_b from './screens/screen1_b';
+
 const Stack = createNativeStackNavigator();
+
 export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName='firstScreen' screenOptions={{ headerShown: false }}>
-        <Stack.Screen name='firstScreen' component={screen1_a} />
+        <Stack.Screen name='firstScreen' component={screen1_b} />
       </Stack.Navigator>
     </NavigationContainer>
   );
