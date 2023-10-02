8f2dd5b6c94b6ff8483b6bac68ab913a1f7fc89e
MinhTam <lait2553@gmail.com>
2023-10-02 21:50:56 +0700

    feat: Add screen1_e component

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ import screen1_a from './screens/screen1_a';
 import screen1_b from './screens/screen1_b';
 import screen1_c from './screens/screen1_c';
 import screen1_d from './screens/screen1_d';
+import screen1_e from './screens/screen1_e';
 
 const Stack = createNativeStackNavigator();
 
@@ -13,7 +14,7 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName='firstScreen' screenOptions={{ headerShown: false }}>
-        <Stack.Screen name='firstScreen' component={screen1_d} />
+        <Stack.Screen name='firstScreen' component={screen1_e} />
       </Stack.Navigator>
     </NavigationContainer>
   );
